1c18a5b3319b3076f66cfff8ebf8286259edb349
NikolnikoffTheOriginal <nikolnikoffg@gmail.com>
2021-10-25 17:03:30 +0300

    Changed the code so it could count dots and commas

--- app/src/main/java/com/example/lab_2/CountingWords.java
+++ app/src/main/java/com/example/lab_2/CountingWords.java
@@ -8,10 +8,27 @@ public class CountingWords {
         if (input == null || input.isEmpty()) {
             return 0;
         }
-        String[] words = input.split("\\s+");
-        return words.length;
+        int wordCount = 0;
+        boolean isWord = false;
+        int endOfLine = input.length() - 1;
+        char[] characters = input.toCharArray();
+        for (int i = 0; i < characters.length; i++) {
+            if (Character.isLetter(characters[i]) && i != endOfLine){
+                isWord = true;
+            } else if (!Character.isLetter(characters[i]) && isWord) {
+                wordCount++;
+                isWord = false;
+            }
+            else if (Character.isLetter(characters[i]) && i == endOfLine) {
+                wordCount++;
+            }
+        }
+        return wordCount;
     }
+
 }
+
+
 
 
 
